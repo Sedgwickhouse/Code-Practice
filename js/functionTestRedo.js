3ce01a3fcefd4233c4d3d9e495e134179d13f7a5
Sedgwick House <sedgwickhouse@Sedgwicks-MacBook-Pro.local>
2020-08-28 12:59:56 -0500

    js for functions practice test

---
+++ js/functionTestRedo.js
@@ -0,0 +1,70 @@
+"use strict";
+
+/**
+ * Write your solutions here.
+ *
+ * **Note**: While normally it is good practice to wrap your javascript in an
+ * immediately invoked function expression (iife), you should _not_ do that
+ * here. If you do, the automated tests will not be able to see your functions.
+ * If you don't know what an iffe is, don't worry about it :)
+ *
+ * This file is for defining the following functions. See README.md for more detail.
+ * isTrue
+ * isFalse
+ * not
+ * addOne
+ * isEven
+ * isIdentical
+ * isEqual
+ * or
+ * and
+ * concat
+ */
+function isTrue(input) {
+    return input === true
+}
+
+function isFalse(cool) {
+    return cool === false
+}
+
+function not(any) {
+    return !any
+}
+
+function addOne(single) {
+    if(typeof single) {
+        return ++single
+    }
+
+}
+
+function isEven(even) {
+    if(even % 2 === 0) {
+        return true
+    }
+    else {
+        return false
+    }
+
+}
+
+function isIdentical(input1, input2) {
+    if(input1 === input2) {
+        return true
+    }
+    else {
+        return false
+    }
+}
+
+function isEqual(sedgwick1, sedgwick2){
+    if(sedgwick1 == sedgwick2) {
+        return true
+    }
+
+}
+
+function concat(argument1, argument2) {
+    return argument1 + " " + argument2;
+}
