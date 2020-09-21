0e91cfcaba26e257950e977407534c2ac71d18e1
Sedgwick House <sedgwick.house20@gmail.com>
2020-09-21 09:26:36 -0500

    adding practice exercises for: fizz-buzz, es6, and updates t personal site.

--- js/fizz-buzz.js
+++ js/fizz-buzz.js
@@ -16,3 +16,23 @@ function fizzBuzz(num) {
 }
 
 fizzBuzz(30)
+
+// writing fizz-buzz using es6
+
+
+const fizzbuzz = (num) => {
+    for(let i = 1; i <= num; i++) {
+        if (i % 15 === 0) {
+            console.log("fizzbuzz")
+        }
+        else if(i % 5 === 0) {
+            console.log("buzz")
+        }
+        else if(i % 3 === 0) {
+            console.log("buzz")
+        }
+        else {
+            console.log(`${i}`)
+        }
+    }
+}
